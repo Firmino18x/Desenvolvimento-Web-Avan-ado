b217be8f49edfa03382ac9b203db9dbc21ab0280
Firmino18x <joaomiguelfirmino16@gmail.com>
2026-03-17 21:35:06 -0300

    Add files via upload

---
+++ aula3.js
@@ -0,0 +1,14 @@
+const pedidos = [
+  { id: 1, cliente: "Ana", total: 120, status: "aprovado" },
+  { id: 2, cliente: "Bruno", total: 80, status: "pendente" },
+  { id: 3, cliente: "Ana", total: 200, status: "aprovado" },
+  { id: 4, cliente: "Carlos", total: 50, status: "cancelado" },
+  { id: 5, cliente: "Bruno", total: 150, status: "aprovado" }
+];
+
+const pedidosAprovados = pedidos.filter(pedido => pedido.status === "aprovado");
+
+const nomesClientes = pedidosAprovados.map(pedido => pedido.cliente);
+
+console.log(pedidosAprovados);
+console.log(nomesClientes);
